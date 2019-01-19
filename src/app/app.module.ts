af5e66dd5b6cde64ccad6aa5c1c5b7875188521f
Francis Labbé <lab_f2000@hotmail.com>
2019-01-19 14:29:22 -0500

    add the main components for the angular application, including material cards

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { RouterModule, Routes } from '@angular/router';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+
+import {MatButtonModule, MatCheckboxModule} from '@angular/material';
+import { CustomMaterialModule } from './core/material.module';
+
+
+
 
 import { AppComponent } from './app.component';
 
+const appRoutes: Routes = [
+];
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    CustomMaterialModule,
+    RouterModule.forRoot(
+      appRoutes
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
