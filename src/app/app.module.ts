768c4f8092ead315dc8fccf130b180e83ad3bc34
Francis Labbé <lab_f2000@hotmail.com>
2019-01-19 17:47:19 -0500

    add pickup component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,23 +11,28 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './component/login/login.component';
 import { HomeComponent } from './component/home/home.component';
+import { PickupComponent } from './component/pickup/pickup.component';
 
 const appRoutes: Routes = [
   { path: 'login', component: LoginComponent },
-  { path: 'home', component: HomeComponent }
+  { path: 'home', component: HomeComponent },
+  { path: 'pickup', component: PickupComponent}
 ];
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    HomeComponent
+    HomeComponent,
+    PickupComponent
   ],
   imports: [
     BrowserModule,
     CustomMaterialModule,
     FormsModule,
     BrowserAnimationsModule,
+    MatCheckboxModule,
+    MatButtonModule,
     RouterModule.forRoot(
       appRoutes
     )
