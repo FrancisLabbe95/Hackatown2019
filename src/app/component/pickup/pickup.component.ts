0045a4330eac17e4d4eb1c4c80a64f924fdfca13
Francis Labbé <lab_f2000@hotmail.com>
2019-01-19 18:47:17 -0500

    add pickup component

--- src/app/component/pickup/pickup.component.ts
+++ src/app/component/pickup/pickup.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-pickup',
@@ -7,9 +8,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PickupComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
+
+  username: string = "Francis"
 
   ngOnInit() {
+  }
+
+  giveBottles(): void { 
+    this.router.navigateByUrl('/home');
   }
 
 }
