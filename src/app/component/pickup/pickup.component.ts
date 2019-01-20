bc5bed432cd08e3294882e324d59cc9f7787d960
Francis Labbé <lab_f2000@hotmail.com>
2019-01-19 22:56:25 -0500

    GOOGLE KNOWS WHERE WE ARE

--- src/app/component/pickup/pickup.component.ts
+++ src/app/component/pickup/pickup.component.ts
@@ -16,24 +16,68 @@ export class PickupComponent implements OnInit {
 
   constructor(private router: Router) { }
 
-  username: string = "Francis"
+  username: string = "Francis";
+  currentLat: number;
+  currentLong: number;
+  marker: any;
+  myMarker: any;
 
   ngOnInit() {
 
     var mapProp = {
-      center: new google.maps.LatLng(18.5793, 73.8143),
-      zoom: 15,
+      center: new google.maps.LatLng(45.5017, -73.5673),
+      zoom: 12,
       mapTypeId: google.maps.MapTypeId.ROADMAP
     };
     this.map = new google.maps.Map(this.gmapElement.nativeElement, mapProp);
+    }
+
+    giveBottles(): void { 
+      this.router.navigateByUrl('/home');
+    }
+
+    setMapType(mapTypeId: string) {
+      this.map.setMapTypeId(mapTypeId)    
   }
 
-  giveBottles(): void { 
-    this.router.navigateByUrl('/home');
+  findMe() {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition((position) => {
+        this.showPosition(position);
+      });
+    } else {
+      alert("Geolocation is not supported by this browser.");
+    }
   }
 
-  setMapType(mapTypeId: string) {
-    this.map.setMapTypeId(mapTypeId)    
-}
+  showPosition(position) {
+    this.currentLat = position.coords.latitude;
+    this.currentLong = position.coords.longitude;
 
+    let location = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+    this.map.panTo(location);
+
+    if (!this.marker) {
+      this.marker = new google.maps.Marker({
+        position: location,
+        map: this.map,
+        title: 'Got you!'
+      });
+    }
+    else {
+      this.marker.setPosition(location);
+    }
+  }
+/*
+  trackMe() {
+    if (navigator.geolocation) {
+      this.isTracking = true;
+      navigator.geolocation.watchPosition((position) => {
+        this.showTrackingPosition(position);
+      });
+    } else {
+      alert("Geolocation is not supported by this browser.");
+    }
+  }
+*/
 }
