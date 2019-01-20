e6cea5ba35d997e2d20f8c5027cef0479a0047ff
Francis Labbé <lab_f2000@hotmail.com>
2019-01-20 10:02:14 -0500

    add map component

--- src/app/component/pickup/pickup.component.ts
+++ src/app/component/pickup/pickup.component.ts
@@ -21,8 +21,15 @@ export class PickupComponent implements OnInit {
   currentLong: number;
   marker: any;
   myMarker: any;
+  directionsService: any;
+  directionsDisplay: any;
+  showAddress: boolean = false;
 
   ngOnInit() {
+
+    this.showAddress = false;
+    this.directionsService = new google.maps.DirectionsService;
+    this.directionsDisplay = new google.maps.DirectionsRenderer;
 
     var mapProp = {
       center: new google.maps.LatLng(45.5017, -73.5673),
@@ -48,7 +55,65 @@ export class PickupComponent implements OnInit {
     } else {
       alert("Geolocation is not supported by this browser.");
     }
+
+    this.addPositions();
+    this.showAddress = true;
   }
+
+  addPositions() {
+    let location = new google.maps.LatLng(45.5017, -73.5673);
+
+    this.showMarker(location);
+
+    this.directionsDisplay.setMap(this.map);
+    this.calculateAndDisplayRoute(this.directionsService, this.directionsDisplay);
+
+  }
+
+  showMarker(location){
+    this.map.panTo(location);
+    if (!this.myMarker) {
+      this.myMarker = new google.maps.Marker({
+        position: location,
+        map: this.map,
+        title: 'Got you!'
+      });
+    }
+    else {
+      this.myMarker.setPosition(location);
+    }
+  }
+
+  calculateAndDisplayRoute(directionsService, directionsDisplay) {
+
+    var waypts = [];
+    var checkboxArray:any[] = [
+        'winnipeg', 'regina','calgary'
+];
+for (var i = 0; i < checkboxArray.length; i++) {
+
+      waypts.push({
+        location: checkboxArray[i],
+        stopover: true
+      });
+
+  }
+
+  directionsService.route({
+    origin: {lat: 41.85, lng: -87.65},
+    destination: {lat: 49.3, lng: -123.12},
+    waypoints: waypts,
+    optimizeWaypoints: true,
+    travelMode: 'DRIVING'
+  }, function(response, status) {
+    if (status === 'OK') {
+      directionsDisplay.setDirections(response);
+    } else {
+      window.alert('Directions request failed due to ' + status);
+    }
+  });
+}
+  
 
   showPosition(position) {
     this.currentLat = position.coords.latitude;
