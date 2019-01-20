4ddc782c5b71b5b9defc79c2509ad284d110a159
Francis Labbé <lab_f2000@hotmail.com>
2019-01-19 19:52:27 -0500

    add maps for developpers

--- src/app/component/pickup/pickup.component.ts
+++ src/app/component/pickup/pickup.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 
+import { ViewChild } from '@angular/core';
+import { } from '@types/googlemaps';
+
 @Component({
   selector: 'app-pickup',
   templateUrl: './pickup.component.html',
@@ -8,15 +11,29 @@ import { Router } from '@angular/router';
 })
 export class PickupComponent implements OnInit {
 
+  @ViewChild('gmap') gmapElement: any;
+  map: google.maps.Map;
+
   constructor(private router: Router) { }
 
   username: string = "Francis"
 
   ngOnInit() {
+
+    var mapProp = {
+      center: new google.maps.LatLng(18.5793, 73.8143),
+      zoom: 15,
+      mapTypeId: google.maps.MapTypeId.ROADMAP
+    };
+    this.map = new google.maps.Map(this.gmapElement.nativeElement, mapProp);
   }
 
   giveBottles(): void { 
     this.router.navigateByUrl('/home');
   }
+
+  setMapType(mapTypeId: string) {
+    this.map.setMapTypeId(mapTypeId)    
+}
 
 }
